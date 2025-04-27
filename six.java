ad5f8edd13f568b755866f1e12de640ce4254f66
NAVNIT BIHARI <biharinavnit28@gmail.com>
2025-04-27 08:52:11 +0530

    Update six.java

--- six.java
+++ six.java
@@ -1,35 +1,33 @@
-public class six {
-    private static String expandFromCenter(String s, int left, int right) {
+class Solution {
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);      
+            int len2 = expandAroundCenter(s, i, i + 1);   
+            int len = Math.max(len1, len2);
+
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
-        // Return the palindrome substring
-        return s.substring(left + 1, right);
+        return right - left - 1; 
     }
-
-    public static String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) return "";
-
-        String longest = "";
-
-        for (int i = 0; i < s.length(); i++) {
-            // Odd length palindrome
-            String oddPal = expandFromCenter(s, i, i);
-            // Even length palindrome
-            String evenPal = expandFromCenter(s, i, i + 1);
-
-            // Update the longest palindrome
-            if (oddPal.length() > longest.length()) {
-                longest = oddPal;
-            }
-            if (evenPal.length() > longest.length()) {
-                longest = evenPal;
-            }
-        }
-
-        return longest;
-    }
+}
 
     public static void main(String[] args) {
         String input = "babad";
